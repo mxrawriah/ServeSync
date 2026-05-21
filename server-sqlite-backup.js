d441d5378ab66255762d8c8604bd0e6939bfe29f
mxrawriah <mariahdanoog99@gmail.com>
2026-05-22 00:49:36 +0800

    Fix node_modules tracking

--- server-sqlite-backup.js
+++ server-sqlite-backup.js
@@ -530,7 +530,7 @@ function respondWithUserData(userRow, res) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: process.env.GOOGLE_CALLBACK_URL // 
   },
   function(accessToken, refreshToken, profile, done) {
     // Find or create user
