a5b9a48bd8a2b379e51969f9d85326df6a6b514e
mxrawriah <mariahdanoog99@gmail.com>
2026-05-21 23:24:51 +0800

    fix dynamic, callback url for deployment

--- server.js
+++ server.js
@@ -381,10 +381,12 @@ async function respondWithUserData(userRow, res) {
 
 // Passport configuration
 passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback"
-},
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.VERCEL_URL 
+      ? `https://${process.env.VERCEL_URL}/auth/google/callback` 
+      : "http://localhost:3000/auth/google/callback"
+  },
   async function (accessToken, refreshToken, profile, done) {
     const email = profile.emails[0].value;
     const firstName = profile.name.givenName;
